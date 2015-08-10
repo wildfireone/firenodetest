e1ceae0d67b89be3ca2dc5cf578af33f9a9fff28
wildfireone <wildfireone@gmail.com>
2015-08-10 14:31:29 +0100

    mysql test

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $port = "3306";
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Check connection
 if ($conn->connect_error) {
-    echo $conn->connect_error;
+    echo "error!!!";
     die("Connection failed: " . $conn->connect_error);
 }
 
