e8f5c73c9e73d06843c8dc41051738e17de09449
wildfireone <wildfireone@gmail.com>
2015-08-10 15:28:40 +0100

    mysql test

--- upload.php
+++ upload.php
@@ -20,14 +20,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Jane', 'Doe', 'jane@example.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Mary', 'Moe', 'mary@example.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Julie', 'Dooley', 'julie@example.com')";
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 
 $conn->close();
 ?>
