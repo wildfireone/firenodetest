dcbffa3fde0ec3ecab47ecfd7f9a1585e199b69a
wildfireone <wildfireone@gmail.com>
2015-08-10 14:29:28 +0100

    mysql test

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
  * Date: 10/08/2015
  * Time: 14:14
  */
-$servername = "eu-cdbr-azure-north-d.cloudapp.net:3306";
+$servername = "eu-cdbr-azure-north-d.cloudapp.net";
 $username = "b99256e599d9fc";
 $password = "64c23a04";
 $dbname = "rgutest";
-
+$port = "3306";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+ //   mysqli_connect(host,username,password,dbname,port,socket);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
